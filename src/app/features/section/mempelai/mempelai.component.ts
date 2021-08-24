9264adf5907f3a9537c0eb9531ff154a46c3e54b
ahadsenja <hamdanilm96@gmail.com>
2021-08-24 12:22:51 +0800

    feat: create mempelai component

---
+++ src/app/features/section/mempelai/mempelai.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mempelai',
+  templateUrl: './mempelai.component.html',
+  styleUrls: ['./mempelai.component.css']
+})
+export class MempelaiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
